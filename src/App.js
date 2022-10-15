4b5ace8d3c816b87f1bc56a2ae73dfa0ddf6a2d5
Rasheedat <lateefrasheedat14@gmail.com>
2022-10-16 00:33:01 +0400

    CustomContext and Useref

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
 
 //import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import CustomAsync from './hooks/CustomAsync';
-
-
-//import ColorState from './State/ColorState';
-//import Counter from './State/Counter';
-
-//import Hook from './State/Hook';
-//import HookState from './State/HookState';
-
-
-//import Name, {addition, subtraction, division} from "./Components/Name";
-//import Result, {Grade} from './Components/Result';
-//import Destructuring from './Components/Destructuring';
-//import EventComponents, {EventComponent1, EventComponent2, EventComponent3, EventComponent4, EventComponent5} from './Components/EventComponents';
+import CustomUseContext2 from './hooks/CustomUseContext2';
+import CustomUseContext3 from './hooks/CustomUseContext3';
+import CustomUseRef from './hooks/CustomUseRef';
 
 function App() { 
   return (
     <div>
-      <CustomAsync />
-    { /* <Form1 /> */}
-     {/*
-      <BrowserRouter>
-      <Layout>
-      <Routes>
-        <Route path="" element={<HomePage/>} />
-        <Route path="about" element={<AboutUs/>} />
-        <Route path="contact" element={<ContactUs/>} />
-        <Route path="*" element={<PageNotFound/>} />
-      </Routes>
-      </Layout>
-      </BrowserRouter>
-*/}
-      
-      { /*<EventComponents />
-      <EventComponent1 name={"Rash"} /> 
-      <EventComponent2 name={"Lat"} /> 
-      <EventComponent3 /> 
-      <EventComponent4 a={1} /> 
-      <EventComponent5 arr={[1, 3, 4]}/> 
-      <Destructuring/> 
-      <Name/> 
-      <Result name = {"Rasheedat"} score = {70}/> 
-      <Result name = {"Rash"} score = {47}/> 
-      <Result name = {"Rasheed"} score = {57}/> 
-  */}
+    <CustomUseRef />
     </div>
   );
 };
